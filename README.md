d481b77c7f00816d7081da60484fdf454d64c6b5
Immortal Izzy <immortal.izzy@tensorshield.ai>
2025-03-26 22:52:44 +0100

    Add InfoBase, AxonInfo

---
+++ README.md
@@ -0,0 +1,1 @@
+# A lightweight library to build Bittensor applications
--- src/libtensorshield/libtensorshield/types/__init__.py
+++ src/libtensorshield/libtensorshield/types/__init__.py
@@ -1,6 +1,10 @@
+from ._axoninfo import AxonInfo
+from ._infobase import InfoBase
 from ._ss58address import SS58Address
 
 
 __all__: list[str] = [
+    'AxonInfo',
+    'InfoBase',
     'SS58Address'
 ]
