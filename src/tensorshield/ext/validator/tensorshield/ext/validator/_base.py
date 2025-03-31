da71c2243bc45faacfab573b7d3e0e0d4125a35c
Immortal Izzy <immortal.izzy@tensorshield.ai>
2025-03-31 20:02:12 +0200

    Pin versions

--- src/tensorshield/ext/validator/tensorshield/ext/validator/_base.py
+++ src/tensorshield/ext/validator/tensorshield/ext/validator/_base.py
@@ -1,12 +1,6 @@
-from typing import Generic
-from typing import TypeVar
-
 from tensorshield.ext.protocol import Synapse
 from tensorshield.ext.soma import Soma
 
 
-S = TypeVar('S', bound=Synapse)
-
-
-class Validator(Soma[S], Generic[S]):
+class Validator(Soma[Synapse]):
     pass
